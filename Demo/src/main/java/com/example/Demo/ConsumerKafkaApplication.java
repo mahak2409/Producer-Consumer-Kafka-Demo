36f793fa9a17262aa99bad8e5ce45bf28bca96d4
mac <mac@localhost>
2022-12-04 20:37:31 +0530

    Producer-Consumer-Demo

---
+++ Demo/src/main/java/com/example/Demo/ConsumerKafkaApplication.java
@@ -0,0 +1,18 @@
+package com.example.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class ConsumerKafkaApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConsumerKafkaApplication.class, args);
+		
+		
+	}
+
+}
